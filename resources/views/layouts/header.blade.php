68b9751f08c3d605fffe7beda3a067c1fc5cde30
phpner <phpner@gmail.com>
2017-02-22 23:05:26 +0200

    first

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,24 @@
+<div class="container-fluid">
+    <div class="row header">
+        <img class="header-bg" src="/img/header.png" alt="">
+        <img  class="flower-top" src="/img/flower-top.png" alt="">
+        <img class="flower-bottom" src="/img/flower-bottom.png" alt="">
+        <nav class="row menu">
+            <span id="email"><img src="/img/email.png" alt=""> spaket17@gmail.com</span>
+            <span id="phone"><img src="/img/phone-call.png" alt=""> 063-622-82-25 <img src="/img/clock.png" alt=""> режим: 24/7</span>
+        </nav>
+        <div class="row">
+            <p class="header-text">
+            Доставка по всей Украине, по  Киеву бесплатная.
+            </p>
+        </div>
+
+        <button>
+            <span id="s">Скачать</span> <span id="p">прайс лист</span>
+        </button>
+
+
+    </div>
+    <div class="row text-bottom">Если вам необходимо всего лишь несколько пачек и нет нужды покупать целый мешок,
+        то свяжитесь с нами, и мы постараемтся вам помочь.</div>
+</div>
